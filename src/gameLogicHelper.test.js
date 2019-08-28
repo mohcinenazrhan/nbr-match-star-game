a7c7b7b56cf585ef0422ae974533e4c7e4baf5cb
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-08-28 21:19:08 +0100

    (Build verification test): add test to gameLogicHelper.test, should create an array of numbers between 1 and 9

--- src/gameLogicHelper.test.js
+++ src/gameLogicHelper.test.js
@@ -3,3 +3,7 @@ import { utils } from './gameLogicHelper';
 it('should calculate the sum of array numbers correctly', () => {
 	expect(utils.sum([ 2, 3, 3, 5, 6, 6, 7 ])).toEqual(32);
 });
+
+it('should create an array of numbers between 1 and 9', () => {
+	expect(utils.range(1, 9)).toEqual([ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]);
+});
