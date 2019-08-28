7422d420850d77fb5b1108a21f327b8e4edc6f69
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-08-28 21:19:27 +0100

    (Build verification test): add test to gameLogicHelper.test, should pick a random number between 1 and 9

--- src/gameLogicHelper.test.js
+++ src/gameLogicHelper.test.js
@@ -7,3 +7,9 @@ it('should calculate the sum of array numbers correctly', () => {
 it('should create an array of numbers between 1 and 9', () => {
 	expect(utils.range(1, 9)).toEqual([ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]);
 });
+
+it('should pick a random number between 1 and 9', () => {
+	const randomNumber = utils.random(1, 9);
+	expect(randomNumber).toBeGreaterThan(0);
+	expect(randomNumber).toBeLessThan(10);
+});
