6c7540df0eff7534ca04d681c735eea427fd583d
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-08-28 21:19:56 +0100

    (Build verification test): add test to gameLogicHelper.test, should pick a random sum (< max) from the set of all available sums given

--- src/gameLogicHelper.test.js
+++ src/gameLogicHelper.test.js
@@ -13,3 +13,10 @@ it('should pick a random number between 1 and 9', () => {
 	expect(randomNumber).toBeGreaterThan(0);
 	expect(randomNumber).toBeLessThan(10);
 });
+
+it('should pick a random sum (< max) from the set of all available sums given', () => {
+	const sums = [ 10, 12, 15, 2, 5 ],
+		max = 9;
+	const randomSum = utils.randomSumIn(sums, max);
+	expect(randomSum === 2 || randomSum === 5).toBeTruthy();
+});
